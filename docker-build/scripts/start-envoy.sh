da27f60491d3b2df106466d1ddec17be2f6fd625
goflutterjava <goflutterjava@gmail.com>
2023-01-03 18:49:52 +0800

    if file exists, just start

--- docker-build/scripts/start-envoy.sh
+++ docker-build/scripts/start-envoy.sh
@@ -3,7 +3,11 @@
 mkdir -p $ENVOY_HOME/logs
 
 CONF=$ENVOY_HOME/envoy.yaml
-echo "admin:" >$CONF
-echo "  address:" >>$CONF
-echo "    socket_address: { address: 0.0.0.0, port_value: 9901 }" >> $CONF
+if [ -f "$CONF" ]; then
+  echo "$CONF exists, just start"
+else
+  echo "admin:" >$CONF
+  echo "  address:" >>$CONF
+  echo "    socket_address: { address: 0.0.0.0, port_value: 9901 }" >> $CONF
+fi
 envoy --config-path $config_path
